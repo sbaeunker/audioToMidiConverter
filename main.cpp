09a42da1616601469ce12d053e53721a1b476445
tot92 <tobi-theis@gmx.de>
2018-12-02 19:14:08 +0100

    started cleaning up main.cpp

--- main.cpp
+++ main.cpp
@@ -6,77 +6,87 @@
 #include "lib/midi-parser.h"
 #include "lib/frequencyApprox.h"
 
+/* DEFAULT PARAMETER */
+const int WINDOW_SIZE = 1000;
+const int WINDOW_DISTANCE = 200;
+const int ZERO_PADDING = 0;
+const int MAX_NOTES = 88;			 // limit midi messages per time unit to enable playing it with midi hardware interface
+const int NOTE_SWITCH_THRESHOLD = 4; // don't switch on too similar notes ... less midi Messages
+const int MIN_VOLUME = 5;			 //don't play too silent notes ... less midi Messages
+const uint32_t TEMPO = 300 * 1000;   // microseconds for one quarter note / beat (default = 500 ms = 500 * 1000 us)
+const uint16_t PPQ = 460;			 // parts per quarter note -> with default settings (quarter note = 500 ms) PPQ *2(!) = parts per second
+
 using namespace std;
 typedef unsigned char uchar;
 
 int main(int argc, char *argv[])
 {
-	string inputFilepath = "";
-	int windowSize = 1000;
-	int windowDistance = 200;
-	int zeroPadding = 0;
-	int maxNotes = 88;			 //limit midi messages per time unit to enable playing it with midi hardware interface
-	int noteSwitchThreshold = 4; // don't switch on too similar notes ... less midi Messages
-	int minVelocity = 5;		 //don't play too silent notes ... less midi Messages
-	bool writeCSV = true;
+	// input validation
 	if (argc < 2)
 	{
-		cerr << "At least one input Argument expected. Specify the File to Convert" << endl;
+		cerr << "At least one input argument is expected. Please provide a file path..." << endl;
 		return -1;
 	}
-	else if (argc > 7)
+	if (argc > 6)
 	{
-		cerr << "Too many Input Arguments" << endl;
+		cerr << "Too many input arguments (" << argc << ")" << endl;
 		return -1;
 	}
-	else
-	{
-		if (argc >= 1)
-		{ //TODO: switch case for readability
-			inputFilepath = string(argv[1]);
-		}
-		if (argc >= 2)
-		{
-			windowSize = atoi(argv[2]);
-		}
-		if (argc >= 3)
-		{
-			windowDistance = atoi(argv[3]);
-		}
-		if (argc >= 4)
-		{
-			zeroPadding = atoi(argv[4]);
-		}
-		if (argc >= 5)
-		{
-			maxNotes = atoi(argv[5]);
-		}
-		//writeCSV = argv[6];
-	}
-	std::cout << "generating: " << inputFilepath << " ,windowSize " << std::to_string(windowSize) << " ,windowDistance " << std::to_string(windowDistance);
-	std::cout << ", zeroPadding TBD "
-			  << ", maxNotes " << std::to_string(maxNotes) << std::endl;
 
+	string inputFilepath = "";
+	int windowSize = WINDOW_SIZE;
+	int windowDistance = WINDOW_DISTANCE;
+	int zeroPadding = ZERO_PADDING;
+	int maxNotes = MAX_NOTES;
+	int noteSwitchThreshold = NOTE_SWITCH_THRESHOLD;
+	int minVolume = MIN_VOLUME;
+
+	// read args
+	for (int i = 0; i < argc; ++i)
+	{
+		char *value = argv[i];
+		switch (i)
+		{
+		case 1:
+			inputFilepath = string(value);
+			break;
+		case 2:
+			windowSize = atoi(value);
+			break;
+		case 3:
+			windowDistance = atoi(value);
+			break;
+		case 4:
+			zeroPadding = atoi(value);
+			break;
+		case 5:
+			maxNotes = atoi(value);
+			break;
+		}
+	}
+	cout << "generating: " << inputFilepath << ", windowSize " << to_string(windowSize) << ", windowDistance " << to_string(windowDistance);
+	cout << ", zeroPadding " << to_string(zeroPadding) << ", maxNotes " << to_string(maxNotes) << endl;
+
+	// read audio file, perform fast-fourier-transformation (FFT) and aggregate results to MIDI compatible format
 	short **midiTable;
 	FrequencyApprox approx{};
-	int frames;
-	int midiTempo;
-	midiTable = approx.toMIDI(argv[1], windowSize, windowDistance, zeroPadding, writeCSV, midiTempo, frames);
+	int frames, midiTempo;
+	midiTable = approx.toMIDI(inputFilepath.c_str(), windowSize, windowDistance, zeroPadding, true, midiTempo, frames);
 
-	std::cout << "save as MIDI file" << std::endl;
-	uint16_t ppq = 460;
+	// parse data to MIDI object
 	uint32_t tempo = midiTempo;
-	MIDIParser parse{tempo, ppq};
-	MIDITrack track = parse.getMidiTrack(midiTable, frames, maxNotes, noteSwitchThreshold, minVelocity, 0, 0);
+	MIDIParser parse{tempo, PPQ};
+	MIDITrack track = parse.getMidiTrack(midiTable, frames, maxNotes, noteSwitchThreshold, minVolume, 0, 0);
 
 	MIDIFile result;
 	result.addTrack(track);
 	result.generate();
 
 	// save as MIDI file
-	std::cout << "save as MIDI file" << std::endl;
-	size_t tempIndex = inputFilepath.find_last_of("/\\");
-	string inputFilename = inputFilepath.substr(tempIndex + 1);
-	string outputFilename = "./output/" + inputFilename.substr(0, inputFilename.length() - 4) + ".mid";
+	size_t slashIndex = inputFilepath.find_last_of("/\\");
+	size_t dotIndex = inputFilepath.find_last_of(".");
+	// get file name without parent directories and file extension
+	string inputFilename = inputFilepath.substr(slashIndex + 1, dotIndex - slashIndex - 1);
+	string outputFilename = "./output/" + inputFilename + ".mid";
 	result.saveAs(outputFilename.c_str());
 }
